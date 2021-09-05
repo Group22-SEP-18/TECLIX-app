30744879532e0d3061c4913bf6ee02170091e065
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-05 18:07:45 +0530

    temp data added

---
+++ lib/data/temporary/customer_search_results.dart
@@ -0,0 +1,16 @@
+import 'package:teclix/data/models/Customer.dart';
+
+List<Customer> customerResults = [
+  Customer.searchResult(
+    shopName: 'abc stores',
+    ownerLastName: 'perera',
+    ownerFistName: 'saman',
+    profilePicUrl: 'static/images/profile_placeholder.png',
+  ),
+  Customer.searchResult(
+    shopName: 'scd stores',
+    ownerLastName: 'peries',
+    ownerFistName: 'kamal',
+    profilePicUrl: 'static/images/profile_placeholder.png',
+  ),
+];
