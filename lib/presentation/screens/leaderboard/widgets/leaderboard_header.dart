e9148691e646bfa47f2bb3c36bc20ad6e8f5be76
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-06 20:13:45 +0530

    leaderboard fix

--- lib/presentation/screens/leaderboard/widgets/leaderboard_header.dart
+++ lib/presentation/screens/leaderboard/widgets/leaderboard_header.dart
@@ -4,7 +4,7 @@ class LeaderboardHeaderClipPath extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
-    path.lineTo(0.0, size.height - 60);
+    path.lineTo(0.0, size.height - 40);
     var firstControlPoint = Offset(0.0, size.height);
     var firstEndPoint = Offset(size.width / 6, size.height);
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
@@ -12,7 +12,7 @@ class LeaderboardHeaderClipPath extends CustomClipper<Path> {
     path.lineTo(5 * size.width / 6, size.height);
 
     var secondControlPoint = Offset(size.width, size.height);
-    var secondEndPoint = Offset(size.width, size.height - 60);
+    var secondEndPoint = Offset(size.width, size.height - 40);
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondEndPoint.dx, secondEndPoint.dy);
 
