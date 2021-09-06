82cb44623c8152c011981e12022ce7a4848c2a56
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-06 17:07:28 +0530

    page routes refactored

--- lib/presentation/screens/start_up.dart
+++ lib/presentation/screens/start_up.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:teclix/logic/bloc/root/root_bloc.dart';
 import 'package:teclix/logic/bloc/root/root_state.dart';
 import 'package:teclix/presentation/screens/navbar/navbar_controller.dart';
-import 'package:teclix/presentation/screens/signin/signin_page.dart';
 import 'package:teclix/presentation/screens/signin/welcome_page.dart';
 
 class StartUpPage extends StatelessWidget {
@@ -15,14 +14,15 @@ class StartUpPage extends StatelessWidget {
       body: BlocConsumer<RootBloc, RootState>(
         listenWhen: (prev, cur) => prev.userLoginState != cur.userLoginState,
         listener: (context, state) {
+          print('state');
+          print(state.userLoginState);
+
           switch (state.userLoginState) {
             case UserLoginState.LOGGED_IN:
-              //TODO: change this to home page logged in
-
-              Navigator.pushReplacementNamed(context, SignInPage.id);
+              Navigator.pushReplacementNamed(context, NavbarController.id);
               break;
             case UserLoginState.LOGGED_OUT:
-              Navigator.pushReplacementNamed(context, NavbarController.id);
+              Navigator.pushReplacementNamed(context, WelcomePage.id);
               break;
             default:
               Navigator.pushReplacementNamed(context, WelcomePage.id);
