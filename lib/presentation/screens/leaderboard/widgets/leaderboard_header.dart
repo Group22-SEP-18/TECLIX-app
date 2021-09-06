c47c36e53401f67b6bf8250d078596eb25a2838c
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-06 08:46:34 +0530

    clippath updated

--- lib/presentation/screens/leaderboard/widgets/leaderboard_header.dart
+++ lib/presentation/screens/leaderboard/widgets/leaderboard_header.dart
@@ -4,15 +4,15 @@ class LeaderboardHeaderClipPath extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
-    path.lineTo(0.0, size.height - 70);
+    path.lineTo(0.0, size.height - 60);
     var firstControlPoint = Offset(0.0, size.height);
-    var firstEndPoint = Offset(size.width / 4, size.height);
+    var firstEndPoint = Offset(size.width / 6, size.height);
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
         firstEndPoint.dx, firstEndPoint.dy);
-    path.lineTo(3 * size.width / 4, size.height);
+    path.lineTo(5 * size.width / 6, size.height);
 
     var secondControlPoint = Offset(size.width, size.height);
-    var secondEndPoint = Offset(size.width, size.height - 70);
+    var secondEndPoint = Offset(size.width, size.height - 60);
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondEndPoint.dx, secondEndPoint.dy);
 
@@ -23,5 +23,5 @@ class LeaderboardHeaderClipPath extends CustomClipper<Path> {
   }
 
   @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
 }
