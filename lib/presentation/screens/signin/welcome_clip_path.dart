7cd3811229f781c629982a2af7d5b3ef8adb890a
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-07 18:59:26 +0530

    welcome page done

---
+++ lib/presentation/screens/signin/welcome_clip_path.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WelcomeClipPath extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 4, size.height - 40, size.width / 2, size.height - 20);
+    path.quadraticBezierTo(
+        3 / 4 * size.width, size.height, size.width, size.height - 30);
+    path.lineTo(size.width, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
+}
