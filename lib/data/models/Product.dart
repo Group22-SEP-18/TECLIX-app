0ab257b42d3e060ac36939a08691c9e8cac0a67e
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-13 20:58:59 +0530

    product model done

---
+++ lib/data/models/Product.dart
@@ -0,0 +1,15 @@
+class Product {
+  String productId;
+  String productName;
+  double price;
+  String productImageUrl;
+  int quantity;
+
+  Product(
+    this.productId,
+    this.productName,
+    this.price,
+    this.productImageUrl,
+    this.quantity,
+  );
+}
