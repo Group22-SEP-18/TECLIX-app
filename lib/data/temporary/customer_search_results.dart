2e728401160d81f108bbd60ad1e3af8fe8f40c86
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-13 21:43:43 +0530

    dummy data added

--- lib/data/temporary/customer_search_results.dart
+++ lib/data/temporary/customer_search_results.dart
@@ -1,4 +1,5 @@
 import 'package:teclix/data/models/Customer.dart';
+import 'package:teclix/data/models/CustomerLocation.dart';
 
 List<Customer> customerResults = [
   Customer.searchResult(
@@ -14,3 +15,15 @@ List<Customer> customerResults = [
     profilePicUrl: 'static/images/profile_placeholder.png',
   ),
 ];
+
+Customer selectedCustomer = Customer.selectedCustomer(
+  shopName: 'Gamini Stores Pvt Ltd',
+  ownerFistName: 'Gamini',
+  ownerLastName: 'Perera',
+  address: CustomerLocation.address(
+    street: '39A kamal lane',
+    city: 'Moratuwa',
+    district: 'Colombo',
+  ),
+  loyaltyPoints: 250.0,
+);
