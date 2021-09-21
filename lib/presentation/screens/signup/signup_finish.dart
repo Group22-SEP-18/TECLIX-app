dd0e2c1308c3a9e6984b21a4dcff6f17ab283d44
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-21 13:16:56 +0530

    ui updated

--- lib/presentation/screens/signup/signup_finish.dart
+++ lib/presentation/screens/signup/signup_finish.dart
@@ -4,63 +4,105 @@ import 'package:teclix/logic/bloc/signup/signup_bloc.dart';
 import 'package:teclix/logic/bloc/signup/signup_event.dart';
 import 'package:teclix/logic/bloc/signup/signup_state.dart';
 import 'package:teclix/presentation/common/constants/TeclixColors.dart';
-import 'package:teclix/presentation/common/widgets/text_button.dart';
 import 'package:teclix/presentation/common/widgets/common_padding.dart';
 import 'package:teclix/presentation/common/widgets/rounded_button.dart';
+import 'package:teclix/presentation/common/widgets/text_button.dart';
+import 'package:teclix/presentation/screens/signin/signin_page.dart';
 import 'package:teclix/presentation/screens/signup/widgets/infoText.dart';
 import 'package:teclix/presentation/screens/signup/widgets/main_heading.dart';
 
 class SignupFinish extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    final signupBloc = BlocProvider.of<SignupBloc>(context);
+  Widget build(BuildContext topcontext) {
+    final signupBloc = BlocProvider.of<SignupBloc>(topcontext);
 
     return Expanded(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Column(
-            children: [
-              SizedBox(
-                height: 65.0,
-              ),
-              MainHeading(
-                text: "Confirm Registration",
-                fontSize: 24.0,
-              ),
-              SizedBox(
-                height: 40.0,
-              ),
-              CommonPadding(
-                child: InfoText(
-                  text:
-                      " 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla ex, "
-                      "vitae tempus lacus condimentum vel. Praesent semper, "
-                      "lectus in dictum scelerisque, erat Leo viverra sem, non sagittis est diam ut nisl',",
-                ),
-              ),
-            ],
+          BlocBuilder<SignupBloc, SignupState>(
+            buildWhen: (prev, cur) => prev.loading != cur.loading,
+            builder: (context, state) {
+              return state.loading
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 100.0,
+                        ),
+                        Center(
+                          child: SizedBox(
+                            child: CircularProgressIndicator(),
+                            height: 80.0,
+                            width: 80.0,
+                          ),
+                        ),
+                      ],
+                    )
+                  : Column(
+                      children: [
+                        SizedBox(
+                          height: 65.0,
+                        ),
+                        MainHeading(
+                          text: "Confirm Registration",
+                          fontSize: 24.0,
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        CommonPadding(
+                          child: InfoText(
+                            text:
+                                " 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla ex, "
+                                "vitae tempus lacus condimentum vel. Praesent semper, "
+                                "lectus in dictum scelerisque, erat Leo viverra sem, non sagittis est diam ut nisl',",
+                          ),
+                        ),
+                      ],
+                    );
+            },
           ),
           Spacer(),
-          Column(
-            children: [
-              BlocBuilder<SignupBloc, SignupState>(
-                builder: (context, state) {
-                  return CommonPadding(
-                    child: RoundedButton(
-                      title: 'Finish',
-                      titleColor: Colors.white,
-                      colour: ColorPrimary,
-                      onPressed: () => signupBloc.add(
-                        NextStepEvent(currentStep: state.step),
+          BlocBuilder<SignupBloc, SignupState>(
+            buildWhen: (prev, cur) =>
+                prev.registeredSucessfully != cur.registeredSucessfully,
+            builder: (context, state) {
+              return state.registeredSucessfully
+                  ? CommonPadding(
+                      child: RoundedButton(
+                        title: 'Go to Login',
+                        titleColor: Colors.white,
+                        colour: ColorPrimary,
+                        onPressed: () => Navigator.pushReplacementNamed(
+                            context, SignInPage.id),
                       ),
-                    ),
-                  );
-                },
-              ),
-              TextBtn(text: 'Cancel'),
-            ],
+                    )
+                  : Column(
+                      children: [
+                        BlocBuilder<SignupBloc, SignupState>(
+                          builder: (context, state) {
+                            return CommonPadding(
+                              child: RoundedButton(
+                                title: 'Finish',
+                                titleColor: Colors.white,
+                                colour: ColorPrimary,
+                                onPressed: () => signupBloc.add(
+                                  SubmitUserEvent(
+                                      salesperson: state.salesperson,
+                                      context: context),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        TextBtn(
+                          text: 'Cancel',
+                        ),
+                      ],
+                    );
+            },
           ),
         ],
       ),
