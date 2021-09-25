495c5b5fc0fae5b5e8f778e6449347c17dff13f6
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-25 23:01:59 +0530

    leaderboard done

--- lib/presentation/screens/leaderboard/leaderboard.dart
+++ lib/presentation/screens/leaderboard/leaderboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -7,8 +8,10 @@ import 'package:teclix/logic/bloc/leaderboard/leaderboard_event.dart';
 import 'package:teclix/logic/bloc/leaderboard/leaderboard_state.dart';
 import 'package:teclix/presentation/common/constants/TeclixColors.dart';
 import 'package:teclix/presentation/common/widgets/appbar_heading_text.dart';
+import 'package:teclix/presentation/screens/leaderboard/widgets/LeaderboardCard.dart';
 import 'package:teclix/presentation/screens/leaderboard/widgets/leaderboard_header.dart';
 import 'package:teclix/presentation/screens/leaderboard/widgets/profile_picture_avatar.dart';
+import 'package:intl/intl.dart';
 
 class LeaderboardPage extends StatelessWidget {
   @override
@@ -89,20 +92,26 @@ class LeaderboardPage extends StatelessWidget {
                                   ),
                                   Text(
                                     state.leaderboard.length > 0
-                                        ? state.leaderboard[0]
-                                                .pointsCurrentMonth +
-                                            'pts.'
+                                        ? NumberFormat.currency(name: '')
+                                                .format(double.parse(state
+                                                    .leaderboard[0]
+                                                    .pointsCurrentMonth)) +
+                                            ' pts.'
                                         : '0',
                                     style: TextStyle(
                                         fontSize: 30.0, color: Colors.white),
                                   ),
                                   Text(
                                     state.leaderboard.length > 0
-                                        ? state.leaderboard[0].salesperson
-                                                .firstName +
+                                        ? toBeginningOfSentenceCase(state
+                                                .leaderboard[0]
+                                                .salesperson
+                                                .firstName) +
                                             ' ' +
-                                            state.leaderboard[0].salesperson
-                                                .lastName
+                                            toBeginningOfSentenceCase(state
+                                                .leaderboard[0]
+                                                .salesperson
+                                                .lastName)
                                         : 'name',
                                     style: TextStyle(
                                         fontSize: 18.0, color: Colors.white),
@@ -141,20 +150,26 @@ class LeaderboardPage extends StatelessWidget {
                                   ),
                                   Text(
                                     state.leaderboard.length > 0
-                                        ? state.leaderboard[1]
-                                                .pointsCurrentMonth +
-                                            'pts.'
+                                        ? NumberFormat.currency(name: '')
+                                                .format(double.parse(state
+                                                    .leaderboard[1]
+                                                    .pointsCurrentMonth)) +
+                                            ' pts.'
                                         : '0',
                                     style: TextStyle(
-                                        fontSize: 22.0, color: Colors.white),
+                                        fontSize: 21.0, color: Colors.white),
                                   ),
                                   Text(
                                     state.leaderboard.length > 0
-                                        ? state.leaderboard[1].salesperson
-                                                .firstName +
+                                        ? toBeginningOfSentenceCase(state
+                                                .leaderboard[1]
+                                                .salesperson
+                                                .firstName) +
                                             ' ' +
-                                            state.leaderboard[1].salesperson
-                                                .lastName
+                                            toBeginningOfSentenceCase(state
+                                                .leaderboard[1]
+                                                .salesperson
+                                                .lastName)
                                         : 'name',
                                     style: TextStyle(
                                         fontSize: 13.0, color: Colors.white),
@@ -167,53 +182,54 @@ class LeaderboardPage extends StatelessWidget {
                         Positioned(
                           right: 15.0,
                           top: 100,
-                          child: BlocBuilder<LeaderboardBloc, LeaderboardState>(
-                            builder: (context, state) {
-                              return Column(
-                                children: [
-                                  Text(
-                                    '3',
-                                    style: TextStyle(
-                                        fontSize: 30.0, color: Colors.white70),
-                                  ),
-                                  SizedBox(
-                                    height: 20.0,
-                                  ),
-                                  ProfilePicAvatar(
-                                    circleRadius: 55.0,
-                                    picture: state.leaderboard.length > 0
-                                        ? state.leaderboard[2].salesperson
-                                            .profilePicture
-                                        : UrlConstants.dummyprofileImage,
-                                    blurRad: 10,
-                                    spreadRad: 5,
-                                  ),
-                                  SizedBox(
-                                    height: 12.0,
-                                  ),
-                                  Text(
-                                    state.leaderboard.length > 0
-                                        ? state.leaderboard[2]
-                                                .pointsCurrentMonth +
-                                            'pts.'
-                                        : '0',
-                                    style: TextStyle(
-                                        fontSize: 22.0, color: Colors.white),
-                                  ),
-                                  Text(
-                                    state.leaderboard.length > 0
-                                        ? state.leaderboard[2].salesperson
-                                                .firstName +
-                                            ' ' +
-                                            state.leaderboard[2].salesperson
-                                                .lastName
-                                        : 'name',
-                                    style: TextStyle(
-                                        fontSize: 13.0, color: Colors.white),
-                                  ),
-                                ],
-                              );
-                            },
+                          child: Column(
+                            children: [
+                              Text(
+                                '3',
+                                style: TextStyle(
+                                    fontSize: 30.0, color: Colors.white70),
+                              ),
+                              SizedBox(
+                                height: 20.0,
+                              ),
+                              ProfilePicAvatar(
+                                circleRadius: 55.0,
+                                picture: state.leaderboard.length > 0
+                                    ? state.leaderboard[2].salesperson
+                                        .profilePicture
+                                    : UrlConstants.dummyprofileImage,
+                                blurRad: 10,
+                                spreadRad: 5,
+                              ),
+                              SizedBox(
+                                height: 12.0,
+                              ),
+                              Text(
+                                state.leaderboard.length > 0
+                                    ? NumberFormat.currency(name: '').format(
+                                            double.parse(state.leaderboard[2]
+                                                .pointsCurrentMonth)) +
+                                        ' pts.'
+                                    : '0',
+                                style: TextStyle(
+                                    fontSize: 20.0, color: Colors.white),
+                              ),
+                              Text(
+                                state.leaderboard.length > 0
+                                    ? toBeginningOfSentenceCase(state
+                                            .leaderboard[2]
+                                            .salesperson
+                                            .firstName) +
+                                        ' ' +
+                                        toBeginningOfSentenceCase(state
+                                            .leaderboard[2]
+                                            .salesperson
+                                            .lastName)
+                                    : 'name',
+                                style: TextStyle(
+                                    fontSize: 13.0, color: Colors.white),
+                              ),
+                            ],
                           ),
                         ),
                       ],
@@ -221,78 +237,54 @@ class LeaderboardPage extends StatelessWidget {
             ),
             body: SingleChildScrollView(
               physics: ClampingScrollPhysics(),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  // SizedBox(
-                  //   height: 25.0,
-                  // ),
-                  // LeaderboardCard(
-                  //   cardColor: ColorGold,
-                  //   image: 'static/images/profile_dummy.jpg',
-                  //   name: 'You',
-                  //   points: '5202',
-                  //   rank: '15',
-                  // ),
-                  // SizedBox(
-                  //   height: 10.0,
-                  // ),
-                  // LeaderboardCard(
-                  //   image: 'static/images/profile_dummy.jpg',
-                  //   name: 'Emma Stone',
-                  //   points: '6850',
-                  //   rank: '4',
-                  // ),
-                  // SizedBox(
-                  //   height: 10.0,
-                  // ),
-                  // LeaderboardCard(
-                  //   image: 'static/images/profile_dummy.jpg',
-                  //   name: 'Emma Stone',
-                  //   points: '6850',
-                  //   rank: '5',
-                  // ),
-                  // SizedBox(
-                  //   height: 10.0,
-                  // ),
-                  // LeaderboardCard(
-                  //   image: 'static/images/profile_dummy.jpg',
-                  //   name: 'Emma Stone',
-                  //   points: '6850',
-                  //   rank: '6',
-                  // ),
-                  // SizedBox(
-                  //   height: 10.0,
-                  // ),
-                  // LeaderboardCard(
-                  //   image: 'static/images/profile_dummy.jpg',
-                  //   name: 'Emma Stone',
-                  //   points: '6850',
-                  //   rank: '7',
-                  // ),
-                  // SizedBox(
-                  //   height: 10.0,
-                  // ),
-                  // LeaderboardCard(
-                  //   image: 'static/images/profile_dummy.jpg',
-                  //   name: 'Emma Stone',
-                  //   points: '6850',
-                  //   rank: '8',
-                  // ),
-                  // SizedBox(
-                  //   height: 10.0,
-                  // ),
-                  // LeaderboardCard(
-                  //   image: 'static/images/profile_dummy.jpg',
-                  //   name: 'Emma Stone',
-                  //   points: '6850',
-                  //   rank: '9',
-                  // ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                ],
+              child: BlocBuilder<LeaderboardBloc, LeaderboardState>(
+                buildWhen: (prev, cur) => prev.loadingData != cur.loadingData,
+                builder: (context, state) {
+                  return state.loadingData
+                      ? Center(
+                          child: CircularProgressIndicator(
+                          color: Colors.white,
+                        ))
+                      : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            for (int i = 3; i < state.leaderboard.length; i++)
+                              LeaderboardCard(
+                                cardColor: i % 2 == 1
+                                    ? CupertinoColors.systemGreen
+                                    : ColorPrimary,
+                                image: state.leaderboard.length > 0
+                                    ? state.leaderboard[i].salesperson
+                                        .profilePicture
+                                    : UrlConstants.dummyprofileImage,
+                                name: state.leaderboard.length > 0
+                                    ? toBeginningOfSentenceCase(state
+                                            .leaderboard[i]
+                                            .salesperson
+                                            .firstName) +
+                                        ' ' +
+                                        toBeginningOfSentenceCase(state
+                                            .leaderboard[i]
+                                            .salesperson
+                                            .lastName)
+                                    : 'name',
+                                points: state.leaderboard.length > 0
+                                    ? NumberFormat.currency(name: '').format(
+                                        double.parse(state
+                                            .leaderboard[i].pointsCurrentMonth))
+                                    : '0',
+                                rank: (i + 1).toString().padLeft(2, '0'),
+                              ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                          ],
+                        );
+                },
               ),
             ),
           );
--- lib/presentation/screens/vehicle/widgets/product_info_card.dart
+++ lib/presentation/screens/vehicle/widgets/product_info_card.dart
@@ -18,91 +18,94 @@ class ProductInfoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return VehicleStatCard(
-      cardHeight: 155.0,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-        child: Row(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  right: 10.0,
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10.0),
-                    bottomLeft: Radius.circular(10.0),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: VehicleStatCard(
+        cardHeight: 155.0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    right: 10.0,
                   ),
-                  child: Image.network(
-                    productImage,
-                    height: 150.0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      bottomLeft: Radius.circular(10.0),
+                    ),
+                    child: Image.network(
+                      productImage,
+                      height: 150.0,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Flexible(
-                        child: MainHeading(
-                          text: productName,
-                          fontSize: 21.0,
+              Expanded(
+                flex: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Flexible(
+                          child: MainHeading(
+                            text: productName,
+                            fontSize: 21.0,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Row(
-                    children: [
-                      MainHeading(
-                        text: 'Price :',
-                        fontSize: 20.0,
-                        color: ColorPrimary,
-                      ),
-                      SizedBox(
-                        width: 5.0,
-                      ),
-                      MainHeading(
-                        text: productPrice,
-                        fontSize: 22.0,
-                        color: Colors.grey,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      MainHeading(
-                        text: 'Quantity :',
-                        fontSize: 20.0,
-                        color: ColorPrimary,
-                      ),
-                      SizedBox(
-                        width: 5.0,
-                      ),
-                      MainHeading(
-                        text: productQuantity,
-                        fontSize: 22.0,
-                        color: Colors.grey,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Row(
+                      children: [
+                        MainHeading(
+                          text: 'Price :',
+                          fontSize: 20.0,
+                          color: ColorPrimary,
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        MainHeading(
+                          text: productPrice,
+                          fontSize: 22.0,
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        MainHeading(
+                          text: 'Quantity :',
+                          fontSize: 20.0,
+                          color: ColorPrimary,
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        MainHeading(
+                          text: productQuantity,
+                          fontSize: 22.0,
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
